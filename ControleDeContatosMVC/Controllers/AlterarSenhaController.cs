dbb2f9dbe48cb2e51ebb8cbb652d72f4385bf4cf
Romulo Freitas <rfcontatosvia@gmail.com>
2023-02-13 19:42:24 -0300

    build: View Alterar senha, controller e model.

---
+++ ControleDeContatosMVC/Controllers/AlterarSenhaController.cs
@@ -0,0 +1,33 @@
+﻿using ControleDeContatosMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControleDeContatosMVC.Controllers
+{
+    public class AlterarSenhaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost] 
+        public IActionResult Alterar(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    TempData["MensagemSucesso"] = "Senha alterada com sucesso";
+                    return View("Index", alterarSenhaModel);
+                }
+                return View("Index", alterarSenhaModel);
+            }
+            catch (Exception erro) 
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos alterar sua senha, tente novamente, detalhe do erro: {erro.Message}";
+                return View("Index", alterarSenhaModel);
+            }
+
+        }
+    }
+}
