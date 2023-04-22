cd06ea082efb10233214fe356ca737b495c3fae9
Romulo Freitas <rfcontatosvia@gmail.com>
2023-04-22 19:19:47 -0300

    Starting

---
+++ ControleDeContatosMVC/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using ControleDeContatosMVC.Filters;
+using ControleDeContatosMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace ControleDeContatosMVC.Controllers
+{
+    [PaginaParaUsuarioLogado] //apenas usuário logado acessa
+    public class BlogController : Controller
+    {
+       
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
